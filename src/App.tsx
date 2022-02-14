ba8ddb5b1f2f06a030e002e19826a703e5856307
Ovidiu Schintee <ovidiu.schintee@ibm.com>
2022-02-14 15:03:23 +0200

    SALESP-662 Add React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Calculator from './views/calculator';
+import ReminderForm from './views/reminderform';
 
 import './styles/index.css';
 
 function App() {
-	return <Calculator />;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Calculator />} />
+				<Route path="reminder" element={<ReminderForm />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
