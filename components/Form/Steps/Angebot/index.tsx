0de1d087c2ce17fdf34085bedbb3b3e5bfe71e0b
Anastasiia Koliada <76648505+koliada-max@users.noreply.github.com>
2022-07-08 11:47:40 +0300

    Center Headline #21

--- components/Form/Steps/Angebot/index.tsx
+++ components/Form/Steps/Angebot/index.tsx
@@ -78,21 +78,21 @@ const Angebot = () => {
 				<>
 					<section className="rwm-forms__page-section tw-margin-top">
 						<div className="tw-flex tw-flex-col">
-							<label className="rwm-form__headline">
-								<h1 className="rwm-form__headline">
+							<label className="rwm-form__headline tw-m-auto tw-pb-10">
+								<h1 className="text-title rwm-form__headline tw-leading-[37px] tw-font-bold">
 									Wo befindet sich die zu prüfende
 									Liegenschaft?
 								</h1>
 							</label>
-							<div className="rwm-form__input-container-large tw-grid tw-grid-cols-1 md:tw-grid-cols-2 lg:tw-grid-cols-2 xl:tw-grid-cols-2 tw-justify-between tw-mt-2">
+							<div className="rwm-form__input-container-large tw-grid tw-grid-cols-1 md:tw-grid-cols-2 lg:tw-grid-cols-2 xl:tw-grid-cols-2 tw-justify-between tw-mt-2 tw-w-full">
 								<div className="rwm-form__input-container">
-									<label className="tw-flex tw-font-size-label tw-font">
+									<label className="tw-flex tw-font-light tw-text-[16px] tw-leading-[149.5%] tw-text-[#605E5C] tw-font">
 										Straße*
 									</label>
 									<input
 										type="text"
 										name="streetName"
-										className="rwm-form__input-custom tw-border-2 'focus:tw-ring-transparent"
+										className="rwm-form__input-custom tw-border-[0.5px]  'focus:tw-ring-transparent"
 										value={
 											currentQuestion.answers.find(
 												(answer) =>
@@ -114,7 +114,7 @@ const Angebot = () => {
 									/>
 								</div>
 								<div className="rwm-form__input-container tw-mt-4 md:tw-mt-0 lg:tw-mt-0 xl:tw-mt-0">
-									<label className="tw-flex tw-font-size-label tw-font">
+									<label className="tw-flex tw-font-light tw-text-[16px] tw-leading-[149.5%] tw-text-[#605E5C] tw-font">
 										Hausnummer*
 									</label>
 									<input
@@ -143,9 +143,9 @@ const Angebot = () => {
 									/>
 								</div>
 							</div>
-							<div className="rwm-form__input-container-large tw-grid tw-grid-cols-1 md:tw-grid-cols-2 lg:tw-grid-cols-2 xl:tw-grid-cols-2  tw-mt-6">
+							<div className="rwm-form__input-container-large tw-grid tw-grid-cols-1 md:tw-grid-cols-2 lg:tw-grid-cols-2 xl:tw-grid-cols-2  tw-mt-6 tw-w-full">
 								<div className="rwm-form__input-container">
-									<label className="tw-flex tw-font-size-label tw-font">
+									<label className="tw-flex tw-font-light tw-text-[16px] tw-leading-[149.5%] tw-text-[#605E5C] tw-font">
 										Postleitzahl*
 									</label>
 									<input
@@ -173,7 +173,7 @@ const Angebot = () => {
 									/>
 								</div>
 								<div className="rwm-form__input-container tw-mt-4 md:tw-mt-0 lg:tw-mt-0 xl:tw-mt-0">
-									<label className="tw-flex tw-font-size-label tw-font">
+									<label className="tw-flex tw-font-light tw-text-[16px] tw-leading-[149.5%] tw-text-[#605E5C] tw-font">
 										Wohnort*
 									</label>
 									<input
