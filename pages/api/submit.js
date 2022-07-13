7a680d86d443db25e9809ef1c055e23392538931
Qiong Wu <qiong.wu@gfnork.de>
2022-07-13 03:11:01 +0300

    more cleanup

--- pages/api/submit.js
+++ pages/api/submit.js
@@ -1,5 +1,6 @@
 import formidable from "formidable";
-import fetch, {FormData, File, fileFrom} from 'node-fetch'
+import fetch, {FormData, File, fileFrom, Blob} from 'node-fetch'
+import {readFile} from 'fs/promises';
 
 const API_URL = "https://prod-174.westeurope.logic.azure.com/workflows/6e30f52072f64fd48e06da4ffc824ba4/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=6y4UGb_dGJ0UdvccVjC7MO-Gy4d9rFQUQ9Dq4dOirY0";
 
@@ -11,17 +12,33 @@ export const config = {
 
 export default async (req, res) => {
 	console.log('new request');
-	const form = formidable({multiples: true});
-	form.parse(req, async (err, fields, files) => {
-		console.log(fields);
-		console.log(files);
-		debugger;
+	const form = formidable({});
 
+	form.parse(req, async (err, fields, files) => {
+		const data = await readFile(files.appData.filepath, "utf8");
+		const appData = JSON.parse(data);
+		debugger;
+		console.log(appData);
+
+		let parsedValue = {
+			liegenschaftAdresse: {
+				streetName: appData['questions']['Wo befindet sich die zu prüfende Liegenschaft?'].answers.find((x) => x.name === 'streetName').value,
+				houseNumber: appData['questions']['Wo befindet sich die zu prüfende Liegenschaft?'].answers.find((x) => x.name === 'houseNumber').value,
+				postalCode: appData['questions']['Wo befindet sich die zu prüfende Liegenschaft?'].answers.find((x) => x.name === 'postalCode').value,
+				city: appData['questions']['Wo befindet sich die zu prüfende Liegenschaft?'].answers.find((x) => x.name === 'city').value,
+			}
+		};
+
+
+		const json = JSON.stringify(parsedValue);
+		const blob = new Blob([json], {
+			type: 'application/json'
+		});
 
 		const formData = new FormData()
 		const binary = new Uint8Array([97, 98, 99])
 		const abc = new File([binary], 'abc.txt', {type: 'text/plain'})
-		formData.set('message', fields.appData)
+		formData.set('message', blob)
 		formData.set('file-upload', abc, 'new name.txt')
 		const response = await fetch(API_URL, {method: 'POST', body: formData});
 		console.log(response.status);
